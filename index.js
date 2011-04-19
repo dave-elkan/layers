71e81424fae24658e6c4349dd2c3415c15d4f482
Dave Elkan <dave@edave.net>
2011-04-20 08:36:34 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Layers: require("./lib/layers/layers"),
+    AbstractView: require("./lib/layers/layers/views/AbstractView"),
+    JsonView: require("./lib/layers/layers/views/JsonView")
+};
