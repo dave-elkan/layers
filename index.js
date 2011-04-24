c34457015f78bbc989bd66872acb9781a9721444
Dave Elkan <dave@edave.net>
2011-04-24 22:35:52 +1000

    Abstracted implementation of route setup.
     - Created Express specific route init.
     - Started on Connect specific route init.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-    Layers: require("./lib/layers/layers"),
+    Express: require("./lib/layers/Layers.Express"),
+    Connect: require("./lib/layers/Layers.Connect"),
     AbstractView: require("./lib/layers/layers/views/AbstractView"),
     JsonView: require("./lib/layers/layers/views/JsonView")
 };
