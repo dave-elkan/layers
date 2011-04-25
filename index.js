36f5a0741c930e902df77eb7573e720c2cdc02ee
Dave Elkan <dave@edave.net>
2011-04-25 20:59:37 +1000

    Removed included view layers  - User to define their own layers (node-layers simply wires them up)

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
     Express: require("./lib/layers/Layers.Express"),
-    Connect: require("./lib/layers/Layers.Connect"),
-    AbstractView: require("./lib/layers/layers/views/AbstractView"),
-    JsonView: require("./lib/layers/layers/views/JsonView")
+    Connect: require("./lib/layers/Layers.Connect")
 };
